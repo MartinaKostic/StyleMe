49423ef26dcc73066a9485d3fa702363a9d5fe5f
MartinaKostic <martina.kostic@gmail.com>
2023-09-08 07:56:37 +0200

    styleinspo

--- app/quiz/styleinspo.tsx
+++ app/quiz/styleinspo.tsx
@@ -1,4 +1,7 @@
+import Link from "next/link";
 import React from "react";
+import Image from "next/image";
+import globalStyles from "@/utils/global";
 
 export interface StyleData {
   [x: string]: any;
@@ -20,10 +23,36 @@ interface StyleInspoProps {
 
 const StyleInspo: React.FC<StyleInspoProps> = ({ styleInspo }) => {
   return (
-    <div>
-      <h1>{styleInspo.title}</h1>
-      <p>{styleInspo.body[0].content}</p>
-      {/* Add more elements to display style inspiration */}
+    <div className="mb-20 grid lg:grid-cols-10 sm:grid-rows-10">
+      <div className="col-start-2 col-span-5">
+        <h1 className="my-5 bg-hotpink text-center text-2xl py-1">
+          {styleInspo.title}
+        </h1>
+      </div>
+      <div className="col-start-2 col-span-4 m-5 mx-16">
+        <p>
+          {styleInspo.body[0].content.slice(0, 300)}
+          ...
+        </p>
+        <div className="flex justify-end">
+          <Link href={`/styleinspo/${styleInspo.id}`}>
+            <button className="border my-5 border-text_color h-10 w-[250px] relative hover-button text-lg">
+              <span>More...</span>
+              <style jsx global>
+                {globalStyles}
+              </style>
+            </button>
+          </Link>
+        </div>
+      </div>
+      <div className="h-72 w-128 relative col-start-6 col-span-4">
+        <Image
+          src={`/${styleInspo.image}`}
+          alt="inspo_image"
+          fill={true}
+          style={{ objectFit: "cover" }}
+        ></Image>
+      </div>
     </div>
   );
 };
